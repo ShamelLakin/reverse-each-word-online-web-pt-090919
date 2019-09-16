bc9aaf0f0fc1981892b5dd1086b6bb6fa7ee9978
Shamel Lakin <christlife4u@gmail.com>
2019-09-16 18:54:20 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
-def reverse_each_word(string)
-  string.each.reverse do |string|
+def reverse_each_word(sentence1)
+  sentence1.each.reverse do |sentence1|
    puts "Hello there, and how are you?" 
   end
 end
