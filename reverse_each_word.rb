074bc5b0a0576f545d507bd14d3f092030e6c579
Shamel Lakin <christlife4u@gmail.com>
2019-09-16 19:04:10 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,5 @@
-sentence1 = "Hello there, and how are you?"
-
 def reverse_each_word(sentence1)
+  sentence1 = "Hello there, and how are you?"
   sentence1.collect.reverse do |sentence1|
     puts "Hello there, and how are you?"
   end
